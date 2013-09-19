b1c25312d1cdfb13845ca2ffc949de4b43a54fb1
Nik Daftary <nikdaftary@gmail.com>
2013-09-19 08:55:05 -0500

    Ex7 complete

--- Exercises.js
+++ Exercises.js
@@ -13,7 +13,7 @@ console.log("1 plus 2 is:", add(1, 2));
 console.log("11 plus 4 is:", add(11, 4));
 
 var add = function(a,b){
-	return a+b;
+  return a+b;
 };
 
 console.log("1 plus 2 is:", add(1, 2));
@@ -82,7 +82,7 @@ console.log("Poke count should be 3:", whinyPerson.pokeCount);
 var whinyPerson = {
   pokeCount: 0,
   poke: function() {
-  	this.pokeCount +=1;
+    this.pokeCount +=1;
   }
 };
 
@@ -112,7 +112,7 @@ var computer = {
   active: false
 };
 computer.switchOn = function(){
-	this.active = true;
+  this.active = true;
 };
 
 computer.switchOn();
@@ -121,14 +121,74 @@ console.log("Computer should be active:", computer.active === true);
 
 ------------------------------------------------------------------------------------------------
 
+Individual Exercise #6
+Create a puppy object:
+
+// Original 
+// With a property mood that points to a string 'hungry'
+// With a property feed that points to a function:
+// This function sets the puppy's mood to 'fed'
+// You must use this in your function
+// var puppy = ???;
+
+// console.log("Puppy should be hungry:", puppy.mood);
+// puppy.feed();
+// console.log("Puppy should be fed:", puppy.mood);
 
 
+var puppy = {
+  mood: "hungry"
+};
+
+puppy.feed = function (){
+  this.mood = "fed"
+};
 
 
+console.log("Puppy should be hungry:", puppy.mood);
+puppy.feed();
+console.log("Puppy should be fed:", puppy.mood);
+
+------------------------------------------------------------------------------------------------
+*** **** * *** * ** ** * * * * ** * * * **** * * * ** * * !!!!
+Individual Exercise #7
+
+Write a function (not a constructor) createTrashCan that:
+Takes one parameter trashAmount
+Returns an object that:
+Has one property trash with the value trashAmount
+----
+var can = createTrashCan(2);
+console.log('Trash can should have 2 trash:', can.trash);
+----
 
 
+// var createTrashCan = function(trashAmount){
+//   var trash = this.trashAmount
+// };
 
 
+// var createTrashCan = function(trashAmount){
+//   var trash = 0,
+//   trash = trash + trashAmount;
+// };
+
+// function createTrashCan(trashAmount) {
+//   var trash = this.trashAmount;
+// };
+
+createTrashCan = function(trashAmount){
+  var something = {
+    trash: trashAmount
+  }
+  return something;
+}
+
+var can = createTrashCan(2);
+console.log('Trash can should have 2 trash:', can.trash);
+
+
+------------------------------------------------------------------------------------------------
 
 
 
